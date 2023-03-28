39538844a04454022d6e7af935efbf3d56363da9
Alex Tyshka <atyshka15@gmail.com>
2023-03-28 13:11:14 -0400

    Initial conversation recording based on examples

--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,138 @@
 #include <iostream> 
-#include <stdlib.h>
 #include "speechapi_cxx.h"
-#include <iterator>
+#include <cpr/cpr.h>
+#include <argparse/argparse.hpp>
+#include <filesystem>
+#include <alsa/asoundlib.h>
 
 using namespace Microsoft::CognitiveServices::Speech;
 using namespace Microsoft::CognitiveServices::Speech::Audio;
+using namespace Microsoft::CognitiveServices::Speech::Transcription;
 using namespace std;
 
 std::string GetEnvironmentVariable(const char* name);
 
-int main()
+string create_voice_signature(const string& key, const string& region, const string& wav_file)
 {
-    // This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
-    auto speechKey = GetEnvironmentVariable("SPEECH_KEY");
-    auto speechRegion = GetEnvironmentVariable("SPEECH_REGION");
+    string endpoint = "https://signature." + region + ".cts.speech.microsoft.com/api/v1/Signature/GenerateVoiceSignatureFromByteArray";
+    // Post the audio file to the endpoint
+    cpr::Response r = cpr::Post(cpr::Url{endpoint},
+                                cpr::Header{{"Ocp-Apim-Subscription-Key", key}},
+                                cpr::Body{cpr::File(wav_file)},
+                                cpr::VerifySsl{false});
+    // Return the signature 
+    std::cout << r.text << std::endl;
+    return r.text;
+}
 
-    if ((std::size(speechKey) == 0) || (size(speechRegion) == 0)) {
+string find_azure_kinect()
+{
+    // Loop through all available alsa inputs
+    // print the number of channels for each device
+    // and return the first device with 8 channels
+    
+}
+
+int main(int argc, char* argv[])
+{
+    argparse::ArgumentParser program("multi_user_asr");
+    program.add_argument("--speaker_samples")
+        .help("WAV files for each speaker")
+        .nargs(argparse::nargs_pattern::any);
+
+    try
+    {
+        program.parse_args(argc, argv);
+    }
+    catch (const std::runtime_error& err)
+    {
+        std::cout << err.what() << std::endl;
+        std::cout << program;
+        exit(0);
+    }
+    
+    // This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+    auto speech_key = GetEnvironmentVariable("SPEECH_KEY");
+    auto speech_region = GetEnvironmentVariable("SPEECH_REGION");
+
+    if ((std::size(speech_key) == 0) || (size(speech_region) == 0)) {
         std::cout << "Please set both SPEECH_KEY and SPEECH_REGION environment variables." << std::endl;
         return -1;
     }
 
-    auto speechConfig = SpeechConfig::FromSubscription(speechKey, speechRegion);
-
-    speechConfig->SetSpeechRecognitionLanguage("en-US");
-
-    auto audioConfig = AudioConfig::FromDefaultMicrophoneInput();
-    auto recognizer = SpeechRecognizer::FromConfig(speechConfig, audioConfig);
-
-    std::cout << "Speak into your microphone.\n";
-    auto result = recognizer->RecognizeOnceAsync().get();
-
-    if (result->Reason == ResultReason::RecognizedSpeech)
-    {
-        std::cout << "RECOGNIZED: Text=" << result->Text << std::endl;
+    std::map<std::string, std::string> speaker_signatures;
+    for (auto wav_file : program.get<std::vector<std::string>>("--speaker_samples")) {
+        std::cout << "Generating signature for " << wav_file << std::endl;
+        string signature = create_voice_signature(speech_key, speech_region, wav_file);
+        string stem = std::filesystem::path(wav_file).stem().string();
+        speaker_signatures[signature] = stem;
     }
-    else if (result->Reason == ResultReason::NoMatch)
-    {
-        std::cout << "NOMATCH: Speech could not be recognized." << std::endl;
-    }
-    else if (result->Reason == ResultReason::Canceled)
-    {
-        auto cancellation = CancellationDetails::FromResult(result);
-        std::cout << "CANCELED: Reason=" << (int)cancellation->Reason << std::endl;
 
-        if (cancellation->Reason == CancellationReason::Error)
+    auto speech_config = SpeechConfig::FromSubscription(speech_key, speech_region);
+    speech_config->SetSpeechRecognitionLanguage("en-US");
+    speech_config->SetProperty("ConversationTranscriptionInRoomAndOnline", "true");
+    speech_config->SetProperty("DifferentiateGuestSpeakers", "true");
+    auto audioProcessingOptions = AudioProcessingOptions::Create(
+        AUDIO_INPUT_PROCESSING_NONE,
+        PresetMicrophoneArrayGeometry::Circular7,
+        SpeakerReferenceChannel::LastChannel);
+    auto audio_config = AudioConfig::FromDefaultMicrophoneInput();
+    auto transcriber = Transcription::ConversationTranscriber::FromConfig(audio_config);
+    auto conversation = Transcription::Conversation::CreateConversationAsync(speech_config).get();
+
+    promise<void> recognition_end;
+    transcriber->Transcribing.Connect([](const ConversationTranscriptionEventArgs& e)
+    {
+        cout << "Recognizing:" << e.Result->Text << std::endl;
+    });
+    transcriber->Transcribed.Connect([](const ConversationTranscriptionEventArgs& e)
+    {
+        if (e.Result->Reason == ResultReason::RecognizedSpeech)
         {
-            std::cout << "CANCELED: ErrorCode=" << (int)cancellation->ErrorCode << std::endl;
-            std::cout << "CANCELED: ErrorDetails=" << cancellation->ErrorDetails << std::endl;
-            std::cout << "CANCELED: Did you set the speech resource key and region values?" << std::endl;
+            cout << "Transcribed: Text=" << e.Result->Text << std::endl
+                << "  Offset=" << e.Result->Offset() << std::endl
+                << "  Duration=" << e.Result->Duration() << std::endl
+                << "  UserId=" << e.Result->UserId << std::endl;
         }
+        else if (e.Result->Reason == ResultReason::NoMatch)
+        {
+            cout << "NOMATCH: Speech could not be recognized." << std::endl;
+        }
+    });
+    transcriber->Canceled.Connect([&recognition_end](const ConversationTranscriptionCanceledEventArgs& e)
+    {
+        cout << "CANCELED: Reason=" << (int)e.Reason << std::endl;
+        if (e.Reason == CancellationReason::Error)
+        {
+            cout << "CANCELED: ErrorCode=" << (int)e.ErrorCode << "\n"
+                 << "CANCELED: ErrorDetails=" << e.ErrorDetails << "\n"
+                 << "CANCELED: Did you set the speech resource key and region values?" << std::endl;
+
+            recognition_end.set_value(); // Notify to stop recognition.
+        }
+    });
+    transcriber->SessionStopped.Connect([&recognition_end](const SessionEventArgs& e)
+    {
+        cout << "Session stopped.";
+        recognition_end.set_value(); // Notify to stop recognition.
+    });
+
+    // Add speaker_signatures to the conversation
+    for (auto& [signature, name] : speaker_signatures) {
+        conversation->AddParticipantAsync(Participant::From(name, "en-us", signature)).get();
     }
+
+    transcriber->JoinConversationAsync(conversation).get();
+    cout << "Conversation joined." << std::endl;
+    transcriber->StartTranscribingAsync().wait();
+
+    recognition_end.get_future().wait();
+
+    transcriber->StopTranscribingAsync().wait();
 }
 
 std::string GetEnvironmentVariable(const char* name)
 {
-#if defined(_MSC_VER)
-    size_t requiredSize = 0;
-    (void)getenv_s(&requiredSize, nullptr, 0, name);
-    if (requiredSize == 0)
-    {
-        return "";
-    }
-    auto buffer = std::make_unique<char[]>(requiredSize);
-    (void)getenv_s(&requiredSize, buffer.get(), requiredSize, name);
-    return buffer.get();
-#else
     auto value = getenv(name);
     return value ? value : "";
-#endif
 }
